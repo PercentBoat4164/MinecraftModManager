a3a93ba1ff9f702b52f2cb81fa43f56bda2b2d68
PercentBoat4164 <51725924+PercentBoat4164@users.noreply.github.com>
2023-01-11 09:46:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import time
 
 # Check if compatible with current instance
 if __name__ == "__main__":
-    authedUser = modrinth.Users.AuthenticatedUser('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')
-
     multimc_manager = MultiMC.MultiMC()
     current_instance = multimc_manager.instances["1.19.2"]
     current_instance.infer_all_existing_mods()
