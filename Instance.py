798c80251b7887833817e8623e3564e6dfc704b2
PercentBoat4164 <thadd.schelp@gmail.com>
2023-02-16 14:58:46 -0500

    - Fixed mods marked as Fabric not updating for Quilt instances.

--- Instance.py
+++ Instance.py
@@ -17,7 +17,7 @@ class Instance:
         return self.name + " @ " + self.path + " : " + self._loader + " " + self._game_version
 
     def is_compatible_with(self, version):
-        return self._loader in version["loaders"] and self._game_version in version["game_versions"]
+        return self._loader in version["loaders"] or (self._loader == "quilt" and "fabric" in version["loaders"]) and self._game_version in version["game_versions"]
 
     def _get_mod(self, mod):
         if mod.endswith(".disabled"):
